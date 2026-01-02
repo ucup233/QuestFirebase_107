eaf2bde6eb2ec3eb3aac3fd953b09bcf4e8fe69f
ucup233 <sufhidayat9@gmail.com>
2026-01-02 14:47:57 +0700

    Entry viewmodel

---
+++ app/src/main/java/com/example/week14/viewmodel/EntryViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.week14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.week14.modeldata.DetailSiswa
+import com.example.week14.modeldata.UIStateSiswa
+import com.example.week14.modeldata.toDataSiswa
+import com.example.week14.repositori.RepositorySiswa
+
+class EntryViewModel(private val repositorySiswa: RepositorySiswa) : ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    /* Fungsi untuk memvalidasi input */
+    private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa = UIStateSiswa(
+            detailSiswa = detailSiswa,
+            isEntryValid = validasiInput(detailSiswa)
+        )
+    }
+
+    /* Fungsi untuk menyimpan data yang di-entry */
+    suspend fun addSiswa() {
+        if (validasiInput()) {
+            repositorySiswa.postDataSiswa(uiStateSiswa.detailSiswa.toDataSiswa())
+        }
+    }
+}
