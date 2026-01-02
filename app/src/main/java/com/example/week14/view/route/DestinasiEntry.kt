bd585e2b40256e43322f0d9883ffbbb9ae2d647a
ucup233 <sufhidayat9@gmail.com>
2026-01-02 15:05:51 +0700

    Destinasi entry

---
+++ app/src/main/java/com/example/week14/view/route/DestinasiEntry.kt
@@ -0,0 +1,8 @@
+package com.example.week14.view.route
+
+import com.example.week14.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
