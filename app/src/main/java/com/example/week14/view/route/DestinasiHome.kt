a2e6ca5604467343535957e8874280aac420ab08
ucup233 <sufhidayat9@gmail.com>
2026-01-02 15:05:13 +0700

    Destinasi home

---
+++ app/src/main/java/com/example/week14/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.week14.view.route
+
+import com.example.week14.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
