3da520e287fbb14acbcc48ba58484264741f97d6
ucup233 <sufhidayat9@gmail.com>
2026-01-09 14:27:45 +0700

    Add edit view model

---
+++ app/src/main/java/com/example/week14/viewmodel/EditViewModel.kt
@@ -0,0 +1,51 @@
+package com.example.week14.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.week14.modeldata.DetailSiswa
+import com.example.week14.modeldata.UIStateSiswa
+import com.example.week14.modeldata.toUiStateSiswa
+import com.example.week14.repositori.RepositorySiswa
+import androidx.compose.runtime.setValue
+import com.example.week14.modeldata.toDataSiswa
+import com.example.week14.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+
+
+class EditViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositorySiswa.getSatuSiswa(idSiswa)!!
+                .toUiStateSiswa(true)
+        }
+    }
+
+    fun updateUiState(detailSiswa: DetailSiswa) {
+        uiStateSiswa =
+            UIStateSiswa(
+                detailSiswa = detailSiswa,
+                isEntryValid = validasiInput(detailSiswa)
+            )
+    }
+
+    private fun validasiInput(
+        uiState: DetailSiswa = uiStateSiswa.detailSiswa
+    ): Boolean {
+        return with(uiState) {
+            nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
