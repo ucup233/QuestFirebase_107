ad7054508bfe5def5861c5c62e5a8cbece4cf5b5
ucup233 <sufhidayat9@gmail.com>
2026-01-09 14:27:59 +0700

    Add fungsi edit satu siswa edit view model

--- app/src/main/java/com/example/week14/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/week14/viewmodel/EditViewModel.kt
@@ -49,3 +49,18 @@ class EditViewModel(
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
         }
     }
+
+    suspend fun editSatuSiswa() {
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            try {
+                repositorySiswa.editSatuSiswa(
+                    idSiswa,
+                    uiStateSiswa.detailSiswa.toDataSiswa()
+                )
+                println("Update Sukses: $idSiswa")
+            } catch (e: Exception) {
+                println("Update Error: ${e.message}")
+            }
+        }
+    }
+}
